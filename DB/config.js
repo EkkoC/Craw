0cae913b188a74a22db7d7e695fd6aecf390a5af
EkkoC <46436307+EkkoC@users.noreply.github.com>
2020-01-14 13:33:53 +0800

    Add files via upload

---
+++ DB/config.js
@@ -0,0 +1,25 @@
+
+/*
+host:登入帳號
+user:登入帳號
+password:登入密碼
+database:連線資料庫
+port:port
+*/ 
+/* var config = {
+    host: 'localhost',  
+    user: 'root',       
+    password: '123456', 
+    database:'advertising', 
+    port: 3306              
+  
+}; */
+var config = {
+    host: '192.168.79.30',  
+    user: 'crawler',       
+    password: '@bot5678@AA!!', 
+    database:'crawler', 
+    port: 3306              
+  
+};
+module.exports = config;
